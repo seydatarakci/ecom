c5b3ba835da38780656019e62b097ec596dccfcc
seydatarakci <seydatarakcii@gmail.com>
2021-07-24 11:38:46 +0300

    first commit

---
+++ store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+from .models import *
+# Create your views here.
+def store(request):
+    context = {}
+    return render(request,'store/store.html',context)
+def cart(request):
+    context = {}
+    return render(request,'store/cart.html',context)
+def checkout(request):
+    context = {}
+    return render(request,'store/checkout.html',context)
